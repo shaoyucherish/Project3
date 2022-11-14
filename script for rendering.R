994bed46f867e20d6862e16a4cd12709e8d14109
shaoyucherish <112103478+shaoyucherish@users.noreply.github.com>
2022-11-14 10:32:24 -0700

    Delete script for rendering.R

--- script for rendering.R
+++
@@ -1,5 +0,0 @@
-#Code for rendering file 
-rmarkdown::render("project3.Rmd", 
-                  output_format = "github_document",
-                  output_file = "README.md",
-                  output_options = list(toc=TRUE, toc_depth=1, toc_float=TRUE))
